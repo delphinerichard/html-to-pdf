fbecfb39d288af97328b01a382278ebded1d88dc
Delphine RICHARD <delphine@digital4better.com>
2024-12-01 12:47:13 +0100

    feat: buildPdf function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  StreamableFile,
+} from '@nestjs/common';
+import { ApiResponsePdfWithBuffer, AppService } from './app.service';
+import { UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { File } from 'buffer';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async buildPdf(@UploadedFile() file: File): Promise<StreamableFile> {
+    const response: ApiResponsePdfWithBuffer =
+      await this.appService.buildPdf(file);
+    try {
+      if (response.success) {
+        return new StreamableFile(response.buffer);
+      }
+    } catch (error) {
+      throw new BadRequestException('PDF generation failed', error.message);
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,109 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import * as puppeteer from 'puppeteer';
+export type ApiResponsePdfWithBuffer = {
+  success: boolean;
+  message: string;
+  buffer: Buffer | null;
+};
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleDestroy, OnModuleInit {
+  private browser: puppeteer.Browser | null = null;
+  private readonly messageErrorLaunchBrowser =
+    'Erreur de lancement du navigateur Puppeteer pour de la génération du PDF';
+  private readonly messageErrorCloseBrowser =
+    'Erreur lors de la fermeture du navigateur Puppeteer';
+  private readonly messageErrorInitBrowser =
+    "Le navigateur Puppeteer n'a pas pu être initialisé, une erreur est survenue lors de son lancement";
+  private readonly messageSuccessGeneratePDF =
+    'Le PDF a été généré avec succès';
+
+  constructor() {}
+
+  /**
+   * Initialisation de l'instance de navigateur de Puppeteer (Singleton)
+   */
+  async onModuleInit() {
+    await this.initBrowser();
+  }
+
+  /**
+   * Initialisation du navigateur Puppeteer
+   */
+  private async initBrowser(): Promise<void> {
+    if (!this.browser) {
+      try {
+        this.browser = await puppeteer.launch({
+          headless: true,
+          executablePath: '/usr/bin/google-chrome',
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--no-first-run',
+            '--disable-gpu',
+            '--disable-software-rasterizer',
+            '--disable-dev-shm-usage',
+            '--no-xshm',
+            '--single-process',
+          ],
+          ignoreDefaultArgs: ['--disable-extensions'],
+          timeout: 60000,
+        });
+        Logger.log('Lancement du navigateur Puppeteer');
+      } catch (error) {
+        Logger.error(this.messageErrorLaunchBrowser, error.stack);
+        throw new Error(this.messageErrorLaunchBrowser);
+      }
+    }
+  }
+
+  /**
+   * Génération du PDF
+   * @param data
+   * @returns un objet Buffer ou null
+   */
+  async buildPdf(data): Promise<ApiResponsePdfWithBuffer> {
+    const htmlContent = data.buffer.toString('utf8');
+
+    if (!this.browser) {
+      throw new Error(this.messageErrorInitBrowser);
+    }
+
+    const page = await this.browser.newPage();
+    await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+
+    const pdfBuffer = Buffer.from(
+      await page.pdf({
+        format: 'A4',
+        printBackground: true,
+        margin: { top: '10mm', right: '10mm', bottom: '10mm', left: '10mm' },
+      }),
+    );
+
+    await page.close();
+    return {
+      success: true,
+      message: this.messageSuccessGeneratePDF,
+      buffer: pdfBuffer,
+    };
+  }
+
+  /**
+   * Fonction permettant de s'assurer que le navigateur est bien fermé après l'utilisation
+   */
+  async onModuleDestroy() {
+    if (this.browser) {
+      try {
+        await this.browser.close();
+        Logger.log('Fermeture du navigateur Puppeteer');
+      } catch (error) {
+        Logger.error(this.messageErrorCloseBrowser, error);
+      }
+    }
   }
 }
