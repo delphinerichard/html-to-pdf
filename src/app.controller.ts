ed2543c1558a5424c75deec89cc21cd3f1ba3fa3
jrmdel <39835580+jrmdel@users.noreply.github.com>
2024-12-06 11:03:04 +0100

    Fix/browser timeouts (#4)
    * fix: timeouts when generating pdf
    
    * misc: improve types and clean code
    
    * edit: rollback on logger injection
    
    * 1.1.0

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
 import {
   BadRequestException,
   Controller,
+  Get,
   Post,
   StreamableFile,
 } from '@nestjs/common';
-import { ApiResponsePdfWithBuffer, AppService } from './app.service';
+import { AppService } from './app.service';
 import { UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { File } from 'buffer';
+import { IHealthStatus } from './app.interface';
 
 @Controller()
 export class AppController {
@@ -15,15 +16,19 @@ export class AppController {
 
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  async buildPdf(@UploadedFile() file: File): Promise<StreamableFile> {
-    const response: ApiResponsePdfWithBuffer =
-      await this.appService.buildPdf(file);
+  async buildPdf(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<StreamableFile> {
     try {
-      if (response.success) {
-        return new StreamableFile(response.buffer);
-      }
+      const buffer = await this.appService.buildPdf(file);
+      return new StreamableFile(buffer);
     } catch (error) {
       throw new BadRequestException('PDF generation failed', error.message);
     }
+  }
+
+  @Get('health')
+  checkStatus(): IHealthStatus {
+    return { status: 'OK' };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,36 +1,17 @@
-import {
-  Injectable,
-  Logger,
-  OnModuleDestroy,
-  OnModuleInit,
-} from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import * as puppeteer from 'puppeteer';
-export type ApiResponsePdfWithBuffer = {
-  success: boolean;
-  message: string;
-  buffer: Buffer | null;
-};
 
 @Injectable()
-export class AppService implements OnModuleDestroy, OnModuleInit {
+export class AppService {
+  private readonly logger = new Logger(AppService.name);
   private browser: puppeteer.Browser | null = null;
   private readonly messageErrorLaunchBrowser =
     'Erreur de lancement du navigateur Puppeteer pour de la génération du PDF';
   private readonly messageErrorCloseBrowser =
     'Erreur lors de la fermeture du navigateur Puppeteer';
-  private readonly messageErrorInitBrowser =
-    "Le navigateur Puppeteer n'a pas pu être initialisé, une erreur est survenue lors de son lancement";
-  private readonly messageSuccessGeneratePDF =
-    'Le PDF a été généré avec succès';
+  private readonly messageErrorBuildPdf = 'La génération du PDF a échoué';
 
   constructor() {}
-
-  /**
-   * Initialisation de l'instance de navigateur de Puppeteer (Singleton)
-   */
-  async onModuleInit() {
-    await this.initBrowser();
-  }
 
   /**
    * Initialisation du navigateur Puppeteer
@@ -38,26 +19,21 @@ export class AppService implements OnModuleDestroy, OnModuleInit {
   private async initBrowser(): Promise<void> {
     if (!this.browser) {
       try {
+        this.logger.debug('Init chromium launch');
         this.browser = await puppeteer.launch({
           headless: true,
           executablePath: '/usr/bin/chromium',
           args: [
             '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--no-first-run',
             '--disable-gpu',
-            '--disable-software-rasterizer',
-            '--disable-dev-shm-usage',
-            '--no-xshm',
-            '--single-process',
             '--disable-features=site-per-process',
           ],
           ignoreDefaultArgs: ['--disable-extensions'],
           timeout: 60000,
         });
-        Logger.log('Lancement du navigateur Puppeteer');
+        this.logger.log('Init browser success');
       } catch (error) {
-        Logger.error(this.messageErrorLaunchBrowser, error.stack);
+        this.logger.error(this.messageErrorLaunchBrowser, error.stack);
         throw new Error(this.messageErrorLaunchBrowser);
       }
     }
@@ -66,46 +42,55 @@ export class AppService implements OnModuleDestroy, OnModuleInit {
   /**
    * Génération du PDF
    * @param data
-   * @returns un objet Buffer ou null
+   * @returns un Buffer ou void
    */
-  async buildPdf(data): Promise<ApiResponsePdfWithBuffer> {
-    const htmlContent = data.buffer.toString('utf8');
+  async buildPdf(data: Express.Multer.File): Promise<Buffer> {
+    try {
+      await this.initBrowser();
 
-    if (!this.browser) {
-      throw new Error(this.messageErrorInitBrowser);
-    }
+      this.logger.debug('Build pdf html content');
+      const htmlContent = data.buffer.toString('utf8');
 
-    const page = await this.browser.newPage();
-    await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+      this.logger.debug('Build pdf new page');
+      const page = await this.browser.newPage();
+      this.logger.debug('Build pdf set content');
+      await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
 
-    const pdfBuffer = Buffer.from(
-      await page.pdf({
+      this.logger.debug('Build pdf convert to pdf');
+      const pdfArray = await page.pdf({
         width: 1280,
         height: 1900,
         scale: 1,
         printBackground: true,
         margin: { top: '0mm', right: '0mm', bottom: '0mm', left: '0mm' },
-      }),
-    );
+      });
+      this.logger.debug('Build pdf convert to buffer');
+      const pdfBuffer = Buffer.from(pdfArray);
 
-    await page.close();
-    return {
-      success: true,
-      message: this.messageSuccessGeneratePDF,
-      buffer: pdfBuffer,
-    };
+      this.logger.debug('Build pdf close');
+      await page.close();
+
+      this.logger.log('Build pdf success');
+      return pdfBuffer;
+    } catch (error) {
+      this.logger.error(this.messageErrorBuildPdf, error);
+      throw error;
+    } finally {
+      this.closeBrowser();
+    }
   }
 
   /**
    * Fonction permettant de s'assurer que le navigateur est bien fermé après l'utilisation
    */
-  async onModuleDestroy() {
+  private async closeBrowser() {
     if (this.browser) {
       try {
         await this.browser.close();
-        Logger.log('Fermeture du navigateur Puppeteer');
+        this.browser = null;
+        this.logger.log('Fermeture du navigateur Puppeteer');
       } catch (error) {
-        Logger.error(this.messageErrorCloseBrowser, error);
+        this.logger.error(this.messageErrorCloseBrowser, error);
       }
     }
   }
